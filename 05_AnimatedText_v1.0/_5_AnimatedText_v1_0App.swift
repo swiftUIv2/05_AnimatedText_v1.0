c7790e0e6cd0dcbf8a1d26ca6b8345e77e87e50b
emm <723125@gmail.com>
2021-01-02 12:14:47 +0000

    Initial Commit

---
+++ 05_AnimatedText_v1.0/_5_AnimatedText_v1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  _5_AnimatedText_v1_0App.swift
+//  05_AnimatedText_v1.0
+//
+//  Created by emm on 02/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct _5_AnimatedText_v1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
